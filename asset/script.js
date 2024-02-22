b122a358921c8ac26fe0209f2301bd0045ac0492
AlessioRomano95 <romanalex400@gmail.com>
2024-02-22 23:48:20 +0100

    [add] IDK

--- asset/script.js
+++ asset/script.js
@@ -1,5 +1,14 @@
 const box = document.getElementById('boxsection');
-for(let i=1; i < 101; i++){
+for(let i=1; i <=100; i++){
 boxsection.innerHTML += `
 <div class="box">${i}</div>`
 }
+
+  if( i % 5 === 0 ) {
+  box.innerHTML = 'FizzBuzz'
+  box.classList = 'green'
+  } 
+
+zegtadnn edgtanowsybhugrvbn0up+ W8erfh 08WUAFYH 08w+q83F0RYQA f098FEHY+
+
+// else if{ box.innerHTML = `${i}` }
